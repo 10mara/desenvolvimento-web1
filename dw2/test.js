8fc820bbb652fa87fa53f6af1d72ab239279fef6
10mara <163425297+10mara@users.noreply.github.com>
2025-03-31 14:24:57 -0300

    dw2

---
+++ dw2/test.js
@@ -0,0 +1,5 @@
+let y = "5" + 2 + 3;
+//Será concatenado primeiro o “5” com o 2 em uma string e logo em seguida o essa string com o 3, resuntando em uma nova string “523”;
+
+let x = 2 + 3 + "5";
+//Será somado primeiro o 2 e o 3 e logo em seguida essa soma será concatenada com o “5”, resuntando em uma nova string “55”.
